b6bcb2c3dd744e8cd7a12394b6e7716434648f3f
arpansharma <arpans0995@gmail.com>
2025-02-14 12:53:43 -0600

    Render and command func

--- Player.js
+++ Player.js
@@ -49,4 +49,5 @@ export class Player {
         return playerInfo;
     }
 
+
 }
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import {Player} from "./Player.js";
 import {Place} from "./Place.js";
+import {Helper} from "./Helper.js";
+
 
 var player1 = new Player("Arav",60,["backpack"]);
 
@@ -25,6 +27,25 @@ console.log(player2.displayPlayerInfo());
 player2.items.push("Boots");
 // console.log(player2.displayPlayerInfo());
 
+
+function render(){
+    console.clear();
+    console.log(player1.getPlayerLocation());
+    player1.displayPlayerInfo();
+}
+
+function go(direction) {
+    player1.place = player1.place.exits[direction];
+    render();
+    return "";
+}
+
+function get(){
+    var item = player1.place.items.pop();
+    player1.items.push(item);
+    render();
+    return "";
+}
 
 
 
